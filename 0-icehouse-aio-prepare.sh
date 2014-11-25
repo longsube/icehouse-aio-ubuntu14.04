7f8461b8eb373993572f78f7b744274f642af668
longsube <longsube@gmail.com>
2014-11-25 23:07:39 +0700

    Xoa  /var/lib/ntp/ntp.conf.dhcp

--- 0-icehouse-aio-prepare.sh
+++ 0-icehouse-aio-prepare.sh
@@ -69,6 +69,7 @@ EOF
 echo "net.ipv4.ip_forward = 1" >> /etc/sysctl.conf
 echo "net.ipv4.conf.all.rp_filter=0" >> /etc/sysctl.conf
 echo "net.ipv4.conf.default.rp_filter=0" >> /etc/sysctl.conf
+rm /var/lib/ntp/ntp.conf.dhcp
 sysctl -p
 
 echo "########## Cai dat & cau hinh NTP ##########"
